22f4d6679abbe3eb96bf5f9640b5f27c63ff244e
Zain Aftab <zainaftab44@gmail.com>
2020-05-01 07:51:57 +0500

    ui updated part 2

--- app/src/main/java/com/sleepycoder/isitthree/MainActivity.java
+++ app/src/main/java/com/sleepycoder/isitthree/MainActivity.java
@@ -5,13 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tv_number, tv_lives;
-    Button btn_yes, btn_no;
+    TextView tv_number, tv_lives, tv_message;
+    ImageButton btn_yes, btn_no;
 
     int number = -1;
     int lives = 3;
@@ -23,8 +24,11 @@ public class MainActivity extends AppCompatActivity {
 
         tv_number = findViewById(R.id.number);
         tv_lives = findViewById(R.id.tv_lives);
+        tv_message = findViewById(R.id.tv_message);
         btn_yes = findViewById(R.id.btn_yes);
         btn_no = findViewById(R.id.btn_no);
+
+        tv_message.setText("Press Start to play");
 
         tv_number.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +37,10 @@ public class MainActivity extends AppCompatActivity {
                     generate_number();
                     lives = 3;
                     tv_lives.setText("Lives: " + lives);
+                    tv_message.setText("Is the number a multiple of three?");
+                    btn_yes.setVisibility(View.VISIBLE);
+                    btn_no.setVisibility(View.VISIBLE);
+                    tv_lives.setVisibility(View.VISIBLE);
                 }
             }
         });
@@ -51,7 +59,11 @@ public class MainActivity extends AppCompatActivity {
                 }
                 if (lives == 0) {
                     tv_number.setText("Start");
-                }else{
+                    tv_message.setText("You have lost.\nPress start to play again");
+                    btn_yes.setVisibility(View.GONE);
+                    btn_no.setVisibility(View.GONE);
+                    tv_lives.setVisibility(View.GONE);
+                } else {
                     generate_number();
                 }
             }
@@ -69,8 +81,12 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "Incorrect", Toast.LENGTH_SHORT).show();
                 }
                 if (lives == 0) {
+                    tv_message.setText("You have lost.\nPress start to play again");
                     tv_number.setText("Start");
-                }else{
+                    btn_yes.setVisibility(View.GONE);
+                    btn_no.setVisibility(View.GONE);
+                    tv_lives.setVisibility(View.GONE);
+                } else {
                     generate_number();
                 }
             }
